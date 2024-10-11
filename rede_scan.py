915841637a65e4409b1c50e1cddc9e18a9168945
mgbrouli <47576364+mgbrouli@users.noreply.github.com>
2024-10-11 15:46:56 -0300

    Rede_scan Atualizado
    Rede scan atualizado
    junto com o modulo scan pois as funções se comunicam entre si pois a ordem interfere.

--- rede_scan.py
+++ rede_scan.py
@@ -1,14 +1,20 @@
 import argparse
 from netaddr import iter_iprange
-import os, platform
+import scan as side
 
 argument = argparse.ArgumentParser(prog='rede_scan.py', description='Meu programa pessoal para escanear Redes Pivot lateral.')
 argument.add_argument('-p', type=str, metavar='[Ping]', nargs=2, help='Ip de host, digite o endereço IPV4 do Gatway padrão')
 argument.add_argument('-m', type=str, metavar='[Mascara Subrede]', nargs=1, help='Digite a mascara de subre rede. Por padrão sera 255.255.255.0')
+argument.add_argument('-o', type=str, metavar='Resultado.txt', nargs=1, help="Digite esta função para output, salvar sua pesquisa em um arquivo externo. \n Caso não escolha o nome do arquivo sera automatizado um nome. \n Somente se for utilizado a opção.")
 
 
 result = argument.parse_args()
 print(result.p)
+
+
+
+f1 = side.verifica_sistema()
+
 
 if result.p == None:
     result.p = ['192.168.0.1', '192.168.0.255']
@@ -19,15 +25,16 @@ print(result.m)
 last = str(result.p)
 print(last)
 
-generate = iter_iprange(result.p[0], result.p[1], step=1 )
-for i in generate:
-    print(i) #result of ping
-    
-def ping(host):
-    if platform.system().lower == "windows":
-        ping_str = '-n 1'
+generate = iter_iprange( result.p[0], result.p[1], step=1 )
+paraPing =''
+for ip in generate:
+    if result.o != None:
+        side.resultado(str(result.o[0]), side.ping(str(ip), f1) )
     else:
-        ping_str = "-c 1"
+        side.ping(str(ip), f1)
+    if result.o == None:
+        result.o = 'resultado.txt'
+    
 
-    resposta = os.system(ping + "ping_str" + generate + "" + host)
-    return resposta == 0
+
+        
---
+++ scan.py
@@ -0,0 +1,38 @@
+import os, platform, subprocess
+
+def resultado(arquivo = "resultado.txt", escrever = ''):
+    if not os.path.exists(arquivo):
+        with open(arquivo, 'w') as res:
+            res.write('Resultado de ping scan. \n Criado por: Welligton \n')
+            print(f"Arquivo '{res}' criado com sucesso.")
+    else:
+        with open(arquivo, 'a') as add:
+            add.write(str(f"\n {escrever}"))
+
+def verifica_sistema():
+    sistema = platform.system()
+    if sistema == "Windows":
+        flag = '-n'
+    else:
+        flag = '-c'
+    return flag
+
+
+def ping(host, f1):
+    try:
+        output = subprocess.run(['ping', f1 ,'1', host ], capture_output=True, text=True)
+
+        if output.returncode == 0:
+            print(f"Ping bem-sucedido em {host}")
+            print(output.stderr)
+
+
+        #aqui fica para teste se retornar mensagem de ip ruim
+        """else:
+            print(f"Falha ao pingar {host}")
+            print(output.stderr)"""
+    except Exception as e:
+        print(f"Erro ao tentar pingar {host}:{e}")
+
+    resultado = f"Ping bem-sucedido em {host}"
+    return resultado
